143d60a90c775d1b86ada98a1b0ef92641e9f433
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-03-04 12:11:56 +0530

    rectified a bug in cart, happened because of removing id from the products.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,37 +3,43 @@ import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
-  
+
   const cartContext = {
     cart: [...cart],
     addToCart,
     removeFromCart,
   };
 
-  function addToCart(item) {
-    let flag=0
-    let res = cart.map((product)=>{
-      if(product.id === item.id){
-        product.quantity ++
-        flag=1
-      }
-      return product
-    })
+  console.log("cart => ", cartContext.cart);
 
-    if(flag===0){
-      item = {...item, quantity: 1}
-      setCart([...cart, item])
-    }else{
-      setCart(res)
+  function addToCart(item) {
+    let flag = 0;
+    let res = cart.map((product) => {
+      if (product.id === item.id) {
+        product.quantity++;
+        flag = 1;
+      }
+      return product;
+    });
+
+    if (flag === 0) {
+      item = { ...item, quantity: 1 };
+      setCart((prevCart) => [...prevCart, item]);
+    } else {
+      setCart(res);
     }
   }
 
   function removeFromCart(id) {
-    let res = cart.filter((item)=> item.id !== id)
-    setCart(res)
+    let res = cart.filter((item) => item.id !== id);
+    setCart(res);
   }
 
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
