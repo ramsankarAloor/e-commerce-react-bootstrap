9a63f71356957a8a0e4c00c126e1cd7a6cb07e0d
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-03-04 08:43:40 +0530

    key and index added to product cards

--- src/components/Products/ProductCards.js
+++ src/components/Products/ProductCards.js
@@ -4,28 +4,24 @@ import CartContext from "../../store/cart-context";
 
 const productsArr = [
   {
-    id: 1,
     title: "Colors",
     price: 100,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
   },
 
   {
-    id: 2,
     title: "Black and white Colors",
     price: 50,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
   },
 
   {
-    id: 3,
     title: "Yellow and Black Colors",
     price: 70,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
   },
 
   {
-    id: 4,
     title: "Blue Color",
     price: 100,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
@@ -39,9 +35,9 @@ const ProductCards = (props) => {
     cartcontext.addToCart(item);
   }
 
-  const cards = productsArr.map((product) => {
+  const cards = productsArr.map((product, index) => {
     return (
-      <Col xs={3} key={product.key}>
+      <Col xs={3} key={index}>
         <Card>
           <Card.Header>{product.title}</Card.Header>
           <Card.Img variant="top" src={product.imageUrl} />
