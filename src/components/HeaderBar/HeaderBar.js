967b106a57c4a9a9ae3b055eb6d3a95c5e12993e
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-03-09 12:28:37 +0530

    conditional rendering of header navigations based on isLoggedIn

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -3,8 +3,10 @@ import { Navbar, Container, Nav, Button, Badge } from "react-bootstrap";
 import styles from "./HeaderBar.module.css";
 import { NavLink } from "react-router-dom";
 import CartContext from "../../store/cart-context";
+import AuthContext from "../../store/auth-context";
 
 const HeaderBar = (props) => {
+  const authCtx = useContext(AuthContext);
   const cartcontext = useContext(CartContext);
   const cartArr = [...cartcontext.cart];
   const badgeNum = cartArr.reduce((acc, product) => acc + product.quantity, 0);
@@ -23,21 +25,51 @@ const HeaderBar = (props) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/" exact className="nav-link" activeClassName={styles.active}>
-              Home
-            </NavLink>
-            <NavLink to="/about" className="nav-link" activeClassName={styles.active}>
+            {authCtx.isLoggedIn && (
+              <NavLink
+                to="/"
+                exact
+                className="nav-link"
+                activeClassName={styles.active}
+              >
+                Products
+              </NavLink>
+            )}
+            <NavLink
+              to="/about"
+              className="nav-link"
+              activeClassName={styles.active}
+            >
               About
             </NavLink>
-            <NavLink to="/contact" className="nav-link" activeClassName={styles.active}>Contact</NavLink>
-            <NavLink to="/auth" className="nav-link" activeClassName={styles.active}>
-              Login
+            <NavLink
+              to="/contact"
+              className="nav-link"
+              activeClassName={styles.active}
+            >
+              Contact
             </NavLink>
+            {!authCtx.isLoggedIn && (
+              <NavLink
+                to="/auth"
+                className="nav-link"
+                activeClassName={styles.active}
+              >
+                Login
+              </NavLink>
+            )}
           </Nav>
         </Navbar.Collapse>
-        <Button variant="outline-secondary" onClick={props.onCartClick}>
-          Cart <Badge bg="secondary">{badgeNum}</Badge>
-        </Button>
+        {authCtx.isLoggedIn && (
+          <div style={{ display: "flex", gap: "10px" }}>
+            <Button variant="outline-secondary" onClick={props.onCartClick}>
+              Cart <Badge bg="secondary">{badgeNum}</Badge>
+            </Button>
+            <Button variant="outline-secondary" onClick={authCtx.logout}>
+              Logout
+            </Button>
+          </div>
+        )}
       </Container>
     </Navbar>
   );
